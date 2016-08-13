9d59a97001de873bb2c2a45d8ba762f39b467642
jlucasnsilva <jlucas.nsilva@gmail.com>
2016-08-13 15:12:31 -0300

    blah blah

--- src/br/ufal/ic/wsn/tmcp/simulator/AbstractController.java
+++ src/br/ufal/ic/wsn/tmcp/simulator/AbstractController.java
@@ -3,14 +3,16 @@ package br.ufal.ic.wsn.tmcp.simulator;
 
 import org.graphstream.graph.Node;
 
-public abstract class AbstractController<T> implements Runnable{
+public abstract class AbstractController<T> implements Runnable {
+	
+	private Node node;
 	
 	public AbstractController(Node node){
-		
+		this.node = node;
 		node.setAttribute("controller", this);
 	}
 	
-	public void receive(Node node, T message){
+	public void receive(T message){
 		node.setAttribute("message", message);
 	}	
 }
